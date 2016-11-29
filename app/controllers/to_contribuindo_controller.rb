0be2931540e8e230e3007e2c042783d62b3b8675
Marlon Mendes <marlonacademy@hotmail.com>
2016-11-29 01:52:46 -0200

    Following idiom language: translating to enlglish

--- app/controllers/to_contribuindo_controller.rb
+++ app/controllers/to_contribuindo_controller.rb
@@ -1,14 +1,17 @@
-# Tô Contribuindo controller
-class ToContribuindoController < ApplicationController
+g# Tô Contribuindo controller
+class ImContributingController < ApplicationController
   # Keep user name and email in a project that user post and search for users marked project
   def index
-    toContribuindo = ToContribuindo.all
-    toContribuindo.each do |m|
-      user = User.find_by_id_usuario(m.id_usuario);
-      m.author_name = (user!=nil) ? user.nome_completo : "anonimo"
-      m.author_email = (user!=nil) ? user.email : "anonimo"
-      m.author_profile = (user!=nil) ?user.perfil : "anonimo"
+    ImContributing = ImContributing.all
+    ImContributing.each do |m|
+      user = User.find_user_by_id(m.user_id);
+
+      m.author_name = (user!=nil) ? user.full_name : "anonymous"
+      m.author_email = (user!=nil) ? user.email : "anonymous"
+      m.author_profile = (user!=nil) ? user.profile : "anonymous"
+
     end
     render json: toContribuindo, methods:[:author_name, :author_email, :author_profile]
   end
 end
+
